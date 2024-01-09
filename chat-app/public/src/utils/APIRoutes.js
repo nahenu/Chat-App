e545504f6ed5ae8a267863a507a62ef480bdc0c3
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 16:52:56 +0530

    Update APIRoutes.js

--- chat-app/public/src/utils/APIRoutes.js
+++ chat-app/public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-api-neon.vercel.app";
+export const host = "https://chat-app-jade-three.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
