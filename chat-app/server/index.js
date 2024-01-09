944a48bccba7490253ca0aa8fa591424df8c552c
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 17:05:01 +0530

    Update index.js

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -13,7 +13,12 @@ app.use(cors({
   credentials:true
   
 } ));
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(express.json());
 
 mongoose
